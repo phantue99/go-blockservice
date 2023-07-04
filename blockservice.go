e0c598ed16bc879254d343a7575b5de53b4e2ca6
PHANTUE\AIOZ <tue.phan@aioz.io>
2023-07-04 15:14:32 +0700

    fix log err

--- blockservice.go
+++ blockservice.go
@@ -416,7 +416,7 @@ func appendFiles(files []string, fileRecordId string) ([]File, uint64, error) {
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, 0, fmt.Errorf("server returned status %d, reqURI: %s", resp.StatusCode, req.RequestURI)
+		return nil, 0, fmt.Errorf("server returned status %d, reqURI: %s", resp.StatusCode, req.URL.String())
 	}
 
 	var (
